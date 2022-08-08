6eed9d938e5feae4dc55b327ed7132d3add4f114
hill0ne <jeanj531@gmail.com>
2022-08-08 15:28:26 +0900

    Feat: "추가 sectioin_items, aside 영역"
    - 상품 사진 추가
    - swiper API 추가, 옵션 수정
    - 베스트 아이템 영역 스타일시트 작성
    - aside 시맨틱 마크업과 스타일시트 작성

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".intro", {
+const swiper = new Swiper(".section_main", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -10,4 +10,9 @@ var swiper = new Swiper(".intro", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+const swiper2 = new Swiper(".items", {
+  slidesPerView: 2.2,
+  spaceBetween: 10,
 });
