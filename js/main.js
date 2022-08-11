213a3468aa9bcab8248271c151322ec32910f156
hill0ne <jeanj531@gmail.com>
2022-08-11 11:42:56 +0900

    Feat: "추가 슬라이드 resize 함수"
    - 모바일, 태블릿에 따라 BEST ITEM 슬라이드 노출 개수가 달라지는 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let status = false;
+
+setMobileScreen();
+setTabletScreen();
+
+// 메인 슬라이드 라이브러리
 const swiper = new Swiper(".section_main", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -12,7 +18,29 @@ const swiper = new Swiper(".section_main", {
   },
 });
 
-const swiper2 = new Swiper(".items", {
-  slidesPerView: 2.2,
-  spaceBetween: 10,
+// 모바일 사이즈 일 때 슬라이드 갯수가 2개,
+// 태블릿 사이즈 일 때 슬라이드 갯수가 4개로 resize 되는 함수
+window.addEventListener("resize", (e) => {
+  let screenWidth = e.target.innerWidth;
+  if (screenWidth < 767 && status === false) {
+    setMobileScreen();
+  } else if (screenWidth > 768 && status === true) {
+    setTabletScreen();
+  }
 });
+
+function setMobileScreen() {
+  const swiper2 = new Swiper(".items", {
+    slidesPerView: 2.2,
+    spaceBetween: 10,
+  });
+  status = true;
+}
+
+function setTabletScreen() {
+  const swiper3 = new Swiper(".items", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+  });
+  status = false;
+}
